d409285c97241b921921cca559cf300aed0f5043
beloyten <daoluongduy@gmail.com>
2020-12-19 01:53:20 +0700

    dahsdkas

--- src/views/guest/landingPage/LandingPage.vue
+++ src/views/guest/landingPage/LandingPage.vue
@@ -433,6 +433,26 @@ export default {
             } else if(document.documentElement.scrollTop > 400) {
                 document.head.appendChild(styleContent1);
             }
+            // var header = document.getElementById("header");
+            // var logo = document.getElementById("logo");
+            // var headingItem = document.getElementById("headingItem");
+            // var booking = document.getElementById("booking");
+            // var textBooking = document.getElementById("textBooking");
+            // if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            //     header.style.justifyContent="flex-end";
+            //     logo.style.width = "45%"
+            //     logo.style.justifyContent="flex-start";
+            //     headingItem.style.width = "40%"
+            //     booking.style.width = "5%"
+            //     textBooking.style.display = "none"
+            // } else {
+            //     header.style.justifyContent="center";
+            //     logo.style.width = "20%"
+            //     logo.style.justifyContent="center";
+            //     headingItem.style.width = "60%"
+            //     booking.style.width = "20%"
+            //     textBooking.style.display = "inline"
+            // }
         }
     }
 }
