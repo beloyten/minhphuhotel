9d4823dd0b206d688e091e12a7aebd6ea3013490
Nhatcao <mitomdasem@gmail.com>
2020-12-27 22:37:34 +0700

    cac

--- src/views/admin/service/components/DeleteService.vue
+++ src/views/admin/service/components/DeleteService.vue
@@ -1,18 +1,39 @@
 <template>
     <el-dialog
         title="Warning"
-        :visible.sync="centerDialogVisible"
+        :visible.sync="openDialog"
         width="30%"
         center>
         <span>It should be noted that the content will not be aligned in center by default</span>
         <span slot="footer" class="dialog-footer">
-            <el-button @click="centerDialogVisible = false">Cancel</el-button>
-            <el-button type="danger" @click="centerDialogVisible = false">Delete</el-button>
+            <el-button @click=" close()" >Cancel</el-button>
+            <el-button type="danger" @click="deleteService()">Delete</el-button>
         </span>
     </el-dialog>
 </template>
 <script>
 export default {
-    
+     props: {
+        openDialog: false,
+        service: 0
+    },
+      methods: {
+        close() {
+            console.log("on close: "+ this.openDialog)
+            this.openDialog = false
+            this.$emit('update:openDialog', false)
+        },
+       deleteService() {
+           const id = {id: this.service}
+            this.$store.dispatch('deleteService', id).then(rs => {
+                if(rs.status === 'success') {
+                    this.close();
+                }
+            })
+            setTimeout(() => {
+                this.loading = false
+            }, 300)
+        
+    }}
 }
 </script>
